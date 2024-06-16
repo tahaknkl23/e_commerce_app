cc6585d11b3bdc713e303a8b4eae21d6435dd9b4
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-06-16 23:23:40 +0300

    chore: Add base and categories models

---
+++ lib/main_wrapper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainWrapper extends StatefulWidget {
+  const MainWrapper({super.key});
+
+  @override
+  State<MainWrapper> createState() => _MainWrapperState();
+}
+
+class _MainWrapperState extends State<MainWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+      ),
+    );
+  }
+}
